c0aa580ff687e12a3b9ed9d2581a8245f36ba487
Abdul Manan <abdulmanan4d@gmail.com>
2025-08-22 15:20:22 +0500

    merging

--- resources/views/complaints/show.blade.php
+++ resources/views/complaints/show.blade.php
@@ -118,7 +118,7 @@
                                                 </div>
                                             </div>
 
-                                            @if(complaint->complaint_status == 1)
+                                            @if($complaint->complaint_status == 1)
                                             <div class="form-group row">
                                                 <label class="col-sm-3 col-form-label font-weight-bolder">Remarks</label>
                                                 <div class="col-sm-9">
