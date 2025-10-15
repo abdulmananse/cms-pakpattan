d5050684426db63642ca2dc6791bcfa708431a1b
Abdul Manan <abdulmanan@pitb.gov.pk>
2025-10-15 13:17:55 +0500

    merging

--- resources/views/front-complaints/show.blade.php
+++ resources/views/front-complaints/show.blade.php
@@ -91,109 +91,120 @@
                                 {{ $complaint->description }}
                             </td>
                         </tr>
+
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Location
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $complaint->location }}
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Source
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ optional($complaint->source)->name }}
+                            </td>
+                        </tr>
+                        
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Complaint By
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ optional($complaint->complaint_by)->name }}
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Complaint Status
+                            </th>
+                            <td class="px-6 py-4">
+                                {!! getComplaintStatusBadge($complaint) !!}
+                            </td>
+                        </tr>
+                        
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Attachments
+                            </th>
+                            <td class="px-6 py-4" class="attachments" colspan="3">
+                                @if($complaint->attachment)
+                                    @php
+                                        $ext = strtolower(pathinfo($complaint->attachment, PATHINFO_EXTENSION));
+                                        $videoExt = ['mp4', 'mov', 'avi', 'mkv', 'webm'];
+                                    @endphp
+                                    <a href="{{ asset('storage/complaints/' . $complaint->attachment) }}" target="_blank">
+                                        @if($ext === 'pdf')
+                                            <img src="{{ asset('images/pdf_icon.png') }}" width="120" alt="PDF" />
+                                        @elseif(in_array($ext, $videoExt))
+                                            <img src="{{ asset('images/vlc_icon.png') }}" width="120" alt="PDF" />
+                                        @else
+                                            <img src="{{ asset('storage/complaints/' . $complaint->attachment) }}" width="120" />
+                                        @endif
+                                    </a>
+                                @endif
+
+                                @if($complaint->complaint_status == 1 && $complaint->resolved_attachment)
+                                    @php
+                                        $resolvedExt = strtolower(pathinfo($complaint->resolved_attachment, PATHINFO_EXTENSION));
+                                        $videoExt = ['mp4', 'mov', 'avi', 'mkv', 'webm'];
+                                    @endphp
+                                    <a href="{{ asset('storage/complaints/' . $complaint->resolved_attachment) }}" target="_blank" class="ms-5">
+                                        @if($resolvedExt === 'pdf')
+                                            <img src="{{ asset('images/pdf_icon.png') }}" width="120" alt="PDF" />
+                                        @elseif(in_array($resolvedExt, $videoExt))
+                                            <img src="{{ asset('images/vlc_icon.png') }}" width="120" alt="PDF" />
+                                        @else
+                                            <img src="{{ asset('storage/complaints/' . $complaint->resolved_attachment) }}" width="120" />
+                                        @endif
+                                    </a>
+                                @endif
+                            </td>
+                        </tr>
+
+                        @if($complaint->department_id > 0)
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Department
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ optional($complaint->department)->name }}
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Assigned At
+                            </th>
+                            <td class="px-6 py-4">
+                                @if($complaint->assigned_at)
+                                {{ date('d M h:i A', strtotime($complaint->assigned_at)) }}
+                                @endif
+                            </td>
+                        </tr>
+                        @endif
+
+                        @if($complaint->complaint_status == 1 || $complaint->complaint_status == 3)
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Resolved Remarks
+                            </th>
+                            <td class="px-6 py-4" colspan="3" class="urduLabel">
+                                {{ $complaint->remarks }}
+                            </td>
+                        </tr>
+                        @endif
+                        
+                        @if($complaint->complaint_status == 3)
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Reopened Remarks
+                            </th>
+                            <td class="px-6 py-4" colspan="3" class="urduLabel">
+                                {{ $complaint->reopened_remarks }}
+                            </td>
+                        </tr>
+                        @endif
+
                     </tbody>
                 </table>
             </div>    
         </div>
     </div>
-
-                                                <tr>
-                                                    <th>Location</th>
-                                                    <td class="urduLabel">{{ $complaint->location }}</td>
-                                                    <th>Source</th>
-                                                    <td>{{ optional($complaint->source)->name }}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Complaint By</th>
-                                                    <td>{{ optional($complaint->complaint_by)->name }}</td>
-                                                    <th>Complaint Status</th>
-                                                    <td>{!! getComplaintStatusBadge($complaint) !!}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Attachments</th>
-                                                    <td class="attachments" colspan="3">
-                                                        @if($complaint->attachment)
-                                                            @php
-                                                                $ext = strtolower(pathinfo($complaint->attachment, PATHINFO_EXTENSION));
-                                                                $videoExt = ['mp4', 'mov', 'avi', 'mkv', 'webm'];
-                                                            @endphp
-                                                            <a href="{{ asset('storage/complaints/' . $complaint->attachment) }}" target="_blank">
-                                                                @if($ext === 'pdf')
-                                                                    <img src="{{ asset('images/pdf_icon.png') }}" width="120" alt="PDF" />
-                                                                @elseif(in_array($ext, $videoExt))
-                                                                    <img src="{{ asset('images/vlc_icon.png') }}" width="120" alt="PDF" />
-                                                                @else
-                                                                    <img src="{{ asset('storage/complaints/' . $complaint->attachment) }}" width="120" />
-                                                                @endif
-                                                            </a>
-                                                        @endif
-
-                                                        @if($complaint->complaint_status == 1 && $complaint->resolved_attachment)
-                                                            @php
-                                                                $resolvedExt = strtolower(pathinfo($complaint->resolved_attachment, PATHINFO_EXTENSION));
-                                                                $videoExt = ['mp4', 'mov', 'avi', 'mkv', 'webm'];
-                                                            @endphp
-                                                            <a href="{{ asset('storage/complaints/' . $complaint->resolved_attachment) }}" target="_blank" class="ms-5">
-                                                                @if($resolvedExt === 'pdf')
-                                                                    <img src="{{ asset('images/pdf_icon.png') }}" width="120" alt="PDF" />
-                                                                @elseif(in_array($resolvedExt, $videoExt))
-                                                                    <img src="{{ asset('images/vlc_icon.png') }}" width="120" alt="PDF" />
-                                                                @else
-                                                                    <img src="{{ asset('storage/complaints/' . $complaint->resolved_attachment) }}" width="120" />
-                                                                @endif
-                                                            </a>
-                                                        @endif
-                                                    </td>
-                                                </tr>
-                                                @if($complaint->department_id > 0)
-                                                <tr>
-                                                    <th>Department</th>
-                                                    <td>{{ optional($complaint->department)->name }}</td>
-                                                    <th>Assigned By</th>
-                                                    <td>{{ optional($complaint->assigned_user)->name }} 
-                                                        @if($complaint->assigned_at)
-                                                        ({{ date('d M h:i A', strtotime($complaint->assigned_at)) }})
-                                                        @endif
-                                                    </td>
-                                                </tr>
-                                                @endif
-
-                                                @if($complaint->complaint_status == 1 || $complaint->complaint_status == 3)
-                                                <tr>
-                                                    <th>Resolved By</th>
-                                                    <td>{{ optional($complaint->resolved_user)->name }}</td>
-                                                    <th>Resolved At</th>
-                                                    <td>{{ date('d M h:i A', strtotime($complaint->resolved_at)) }}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Remarks</th>
-                                                    <td colspan="3" class="urduLabel">{{ $complaint->remarks }}</td>
-                                                </tr>
-                                                @endif
-
-                                                @if($complaint->complaint_status == 3)
-                                                <tr>
-                                                    <th>Reopened By</th>
-                                                    <td>{{ optional($complaint->reopened_user)->name }}</td>
-                                                    <th>Reopened Remarks</th>
-                                                    <td class="urduLabel">{{ $complaint->reopened_remarks }}</td>
-                                                </tr>
-                                                @endif
-
-                                            </tbody>
-                                            </table>
-                                        </div>
-
-                                    </div>
-                                <!-- [ basic-table ] end -->
-                            </div>
-                            <!-- [ Main Content ] end -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- [ Main Content ] end -->
-
 </x-app-layout>
