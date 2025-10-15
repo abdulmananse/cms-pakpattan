be39ac32cd2fccdd3165808e9228b1ac4a2bfe4d
Abdul Manan <abdulmanan@pitb.gov.pk>
2025-10-15 12:41:26 +0500

    merging

--- resources/views/front-complaints/show.blade.php
+++ resources/views/front-complaints/show.blade.php
@@ -33,6 +33,104 @@
 
 
 
+    <div class="min-h-auto flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="w-full sm:max-w-xl mt-6 mb-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                                Product name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Color
+                            </th>
+                            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Price
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Microsoft Surface Pro
+                            </th>
+                            <td class="px-6 py-4">
+                                White
+                            </td>
+                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                Laptop PC
+                            </td>
+                            <td class="px-6 py-4">
+                                $1999
+                            </td>
+                        </tr>
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Magic Mouse 2
+                            </th>
+                            <td class="px-6 py-4">
+                                Black
+                            </td>
+                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                Accessories
+                            </td>
+                            <td class="px-6 py-4">
+                                $99
+                            </td>
+                        </tr>
+                        <tr class="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Google Pixel Phone
+                            </th>
+                            <td class="px-6 py-4">
+                                Gray
+                            </td>
+                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                Phone
+                            </td>
+                            <td class="px-6 py-4">
+                                $799
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                Apple Watch 5
+                            </th>
+                            <td class="px-6 py-4">
+                                Red
+                            </td>
+                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                Wearables
+                            </td>
+                            <td class="px-6 py-4">
+                                $999
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>    
+            
+        </div>
+    </div>
     
 
 
@@ -56,98 +154,7 @@
                                         <div class="p-6 bg-white shadow rounded-md ">
 
 
-                                            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                                                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                    <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
-                                                        <tr>
-                                                            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                                                                Product name
-                                                            </th>
-                                                            <th scope="col" class="px-6 py-3">
-                                                                Color
-                                                            </th>
-                                                            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                                                                Category
-                                                            </th>
-                                                            <th scope="col" class="px-6 py-3">
-                                                                Price
-                                                            </th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                                                Apple MacBook Pro 17"
-                                                            </th>
-                                                            <td class="px-6 py-4">
-                                                                Silver
-                                                            </td>
-                                                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                                                Laptop
-                                                            </td>
-                                                            <td class="px-6 py-4">
-                                                                $2999
-                                                            </td>
-                                                        </tr>
-                                                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                                                Microsoft Surface Pro
-                                                            </th>
-                                                            <td class="px-6 py-4">
-                                                                White
-                                                            </td>
-                                                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                                                Laptop PC
-                                                            </td>
-                                                            <td class="px-6 py-4">
-                                                                $1999
-                                                            </td>
-                                                        </tr>
-                                                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                                                Magic Mouse 2
-                                                            </th>
-                                                            <td class="px-6 py-4">
-                                                                Black
-                                                            </td>
-                                                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                                                Accessories
-                                                            </td>
-                                                            <td class="px-6 py-4">
-                                                                $99
-                                                            </td>
-                                                        </tr>
-                                                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                                                Google Pixel Phone
-                                                            </th>
-                                                            <td class="px-6 py-4">
-                                                                Gray
-                                                            </td>
-                                                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                                                Phone
-                                                            </td>
-                                                            <td class="px-6 py-4">
-                                                                $799
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                                                Apple Watch 5
-                                                            </th>
-                                                            <td class="px-6 py-4">
-                                                                Red
-                                                            </td>
-                                                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                                                Wearables
-                                                            </td>
-                                                            <td class="px-6 py-4">
-                                                                $999
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
+                                            
 
 
                                             <table class="min-w-full divide-y divide-gray-200  table-bordered align-middle">
